6784cf0f22716f0215deca37532e197d4bfd8595
zHacks <chanwithacker@gmail.com>
2018-01-13 21:49:15 +0700

    a

--- bot.php
+++ bot.php
@@ -15,14 +15,17 @@ if (!is_null($events['events'])) {
 		//if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
 			$text = $event['message']['text'];
+
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => $text
-			];
+			// $messages = [
+			// 	'type' => 'text',
+			// 	'text' => $text
+			// ];
+
+			$messages = $event;
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
